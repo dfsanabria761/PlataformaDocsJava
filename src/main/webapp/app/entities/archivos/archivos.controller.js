8b2c6428230a960e6e3e585a28c115321a56e6fb
Nicolás Muñoz <nd.munoz10@uniandes.edu.co>
2017-12-03 18:56:12 -0500

    Carpetas de fisica y anadec

--- src/main/webapp/app/entities/archivos/archivos.controller.js
+++ src/main/webapp/app/entities/archivos/archivos.controller.js
@@ -44,8 +44,7 @@
         var fileChooser = document.getElementById('file-chooser');
         var button = document.getElementById('upload-button');
         var results = document.getElementById('results');
-        var fileName = document.getElementById('filename');
-        var materia = document.getElementById('materia');
+        var allItems = document.getElementById('allItems');
         button.addEventListener('click', function() {
             if(fileChooser==null){
                 return;
@@ -72,6 +71,8 @@
             }
             
         }, false);
+
+
         var delimiters = {
             Bucket: "platformadocs",
             Delimiter: "/",
@@ -90,33 +91,95 @@
             $scope.$broadcast("REFRESH");
         });
         */
+        
 
-        $(document).ready(function(){
-        $("#btnAnadec").click(function(){
-        $("#modalAnadec").modal();
-        });
-    });
-
-        bucket.listObjects(delimiters, function (err, response) {
-           
-            if (err) {
-                results.innerHTML = ("Error al subir datos: ", err);
-            } else {
-                for(var i = 0; i < response.Contents.length; i++)
+        bucket.listObjects(delimiters, function (err, response) 
+        {
+            var fileNameAna = '';
+            var fileNameFisi = '';
+            for(var i = 0; i < response.Contents.length; i++)
+            {
+                if(response.Contents[i].Key.startsWith("ANA"))
                 {
-                   if(response.Contents[i].Key.startsWith("ANA"))
-                   {
-                        materia.insertAdjacentHTML('beforeend', 'ANADEC')
-                        fileName.insertAdjacentHTML('beforeend', '<a href="https://s3-us-east-2.amazonaws.com/' 
-                        + delimiters.Bucket + delimiters.Delimiter 
-                        + response.Contents[i].Key 
-                        + '" target="_blank" style="color:#0000FF">'
-                        + response.Contents[i].Key 
-                        + ' </a>');
-                   }
+                    fileNameAna += '<a href="https://s3-us-east-2.amazonaws.com/' 
+                    + delimiters.Bucket 
+                    + delimiters.Delimiter 
+                    + response.Contents[i].Key 
+                    + '" target="_blank" style="color:#0000FF">'
+                    + response.Contents[i].Key 
+                    + ' </a> <br></br>';
                 }
-            }       
+                else if(response.Contents[i].Key.startsWith("FISI"))
+                {
+                    fileNameFisi += '<a href="https://s3-us-east-2.amazonaws.com/' 
+                    + delimiters.Bucket 
+                    + delimiters.Delimiter 
+                    + response.Contents[i].Key 
+                    + '" target="_blank" style="color:#0000FF">'
+                    + response.Contents[i].Key 
+                    + ' </a> <br></br>';
+                }
+            }
+            allItems.insertAdjacentHTML("beforeend", '<div class="col-sm-4 col-md-4"'
+                        + '<div class="post">'
+                        + '<img src="content/images/folder.png" class="img-responsive"/>'
+                        + '<span class="post-title"><b> ANADEC </b><br/> </span>'
+                        + '<button class="btn btn-warning btn-sm" id="btnAnadec" data-toggle="modal" data-target="#modalAnadec">Leer más</button>'
+                        + '<div class="modal fade" id="modalAnadec" role="dialog">'
+                        + '<div class="modal-dialog">'
+                        + '<div class="modal-content">'
+                        + '<div class="modal-header">'
+                        + '<button type="button" class="close" data-dismiss="modal">&times;</button>'
+                        + '<h4 class="modal-title" style="color:#000" >¡Mira los archivos que tenemos para ti!</h4>'
+                        + '</div>'
+                        + '<div class="modal-body">'
+                        + '<div class="row">'
+                        + '<div class=" col-sm-12">'
+                        + '<p style="color:#000" class="list-group-item">' + fileNameAna + '</p>'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>'
+                        + '<div class="modal-footer">'
+                        + '<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>'
+                        + '</div>'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>' 
+                        + '<div class="content">'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>');
+            allItems.insertAdjacentHTML("beforeend", '<div class="col-sm-4 col-md-4"'
+                        + '<div class="post">'
+                        + '<img src="content/images/folder.png" class="img-responsive"/>'
+                        + '<span class="post-title"><b> FÍSICA </b><br/> </span>'
+                        + '<button class="btn btn-warning btn-sm" id="btnFisi" data-toggle="modal" data-target="#modalFisi">Leer más</button>'
+                        + '<div class="modal fade" id="modalFisi" role="dialog">'
+                        + '<div class="modal-dialog">'
+                        + '<div class="modal-content">'
+                        + '<div class="modal-header">'
+                        + '<button type="button" class="close" data-dismiss="modal">&times;</button>'
+                        + '<h4 class="modal-title" style="color:#000" >¡Mira los archivos que tenemos para ti!</h4>'
+                        + '</div>'
+                        + '<div class="modal-body">'
+                        + '<div class="row">'
+                        + '<div class=" col-sm-12">'
+                        + '<p style="color:#000" class="list-group-item">' + fileNameFisi + '</p>'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>'
+                        + '<div class="modal-footer">'
+                        + '<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>'
+                        + '</div>'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>' 
+                        + '<div class="content">'
+                        + ' </div>'
+                        + ' </div>'
+                        + ' </div>');
         });
-
     }
 })();
